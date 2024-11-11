8098ca1d92aed912fe37df386f8ea0ce2e6ef4c7
williammmm <91826947+williamwith4ms@users.noreply.github.com>
2024-11-11 12:39:57 +0000

    updated stage 1

--- Makefile
+++ Makefile
@@ -3,41 +3,50 @@ LD = i686-elf-ld
 NASM = nasm
 OBJCOPY = i686-elf-objcopy
 ASMFLAGS = -f bin
-CFLAGS = -m32 -ffreestanding -fno-builtin -I include
+CFLAGS = -ffreestanding -fno-builtin -I include 
 LDFLAGS = -T linker.ld -m elf_i386
 
 C_SOURCES = $(wildcard src/kernel/*.c)
 ASM_SOURCES = $(wildcard src/bootloader/*.asm)
 
 C_OBJECTS = $(C_SOURCES:src/kernel/%.c=.build/%.o)
-ASM_BINARIES = $(ASM_SOURCES:src/bootloader/%.asm=.build/%.bin)
-ASM_OBJECTS = $(ASM_BINARIES:.bin=.o)
+ASM_BINARIES = $(ASM_SOURCES:src/bootloader/%.asm=.build/%.bin)	
 
-all: .build/boot.bin
+all: .build/boot.img
 
-.build/boot.bin: .build/bootloader_stage_0.bin .build/kernel.bin
-	cat $^ > $@
+.build/boot.img: .build/bootloader_stage_0.bin .build/bootloader_stage_1.bin .build/kernel.bin
+	# create blank disk image
+	dd if=/dev/zero of=.build/boot.img bs=512 count=2880
+
+	# place stage 0 at sector 1
+	dd if=.build/bootloader_stage_0.bin of=.build/boot.img bs=512 count=1 conv=notrunc
+
+	# place stage 1 at sector 2
+	dd if=.build/bootloader_stage_1.bin of=.build/boot.img bs=512 count=1 seek=1 conv=notrunc
+	
+	# place kernel at sector 3 
+	dd if=.build/kernel.bin of=.build/boot.img seek=2 conv=notrunc
 
 .build/kernel.bin: .build/kernel.elf
-	cp .build/kernel.elf .build/kernel.bin
+	$(OBJCOPY) -O binary $< $@
 
-.build/kernel.elf: $(C_OBJECTS) $(ASM_OBJECTS)
-	@echo "Linking kernel.elf with objects: $(C_OBJECTS) $(ASM_OBJECTS)"
+.build/kernel.elf: $(C_OBJECTS)
+	@echo "Linking $^ to $@"
 	$(LD) $(LDFLAGS) -o $@ $^
 
 .build/%.o: src/kernel/%.c
 	@echo "Compiling $< to $@"
 	$(CC) $(CFLAGS) -c $< -o $@
 
-.build/%.bin: src/bootloader/%.asm
+.build/bootloader_stage_0.bin: src/bootloader/bootloader_stage_0.asm
 	@echo "Assembling $< to $@"
 	$(NASM) $(ASMFLAGS) $< -o $@
 
-.build/%.o: .build/%.bin
-	@echo "Converting $< to $@"
-	$(OBJCOPY) -I binary -O elf32-i386 $< $@
+.build/bootloader_stage_1.bin: src/bootloader/bootloader_stage_1.asm
+	@echo "Assembling $< to $@"
+	$(NASM) $(ASMFLAGS) $< -o $@
 
 clean:
-	rm -f .build/*.o .build/*.bin .build/kernel.bin .build/kernel.elf
+	rm -rf .build/*
 
 .PHONY: all clean
--- src/bootloader/bootloader_stage_1.asm
+++ src/bootloader/bootloader_stage_1.asm
@@ -9,83 +9,96 @@ CALL printStr
 ; Print the message "Loaded stage 1"
 
 ; Set stack pointer
-;MOV AX, 0x7C00
-;MOV SS, AX
-;MOV SP, 0x7C00
+MOV AX, 0x7C00
+MOV SS, AX
+MOV SP, 0x7C00
+
+
 
 ; Query BIOS for size of lower memory
-;MOV AX, 0x88
-;INT 0x15
+MOV AX, 0x88
+INT 0x15
 
 ; Query BIOS for size of upper memory
-;MOV AX, 0xE801
-;INT 0x15
+MOV AX, 0xE801
+INT 0x15
 
-; Read kernel sectors to lower memory
-;MOV AH, 0x02
-;MOV AL, 0x01
-;MOV CH, 0x00
-;MOV CL, 0x02
-;MOV DH, 0x00
-;MOV DL, 0x80
-;MOV BX, 0x7E00
-;INT 0x13
-;JC ReadError
+; load kernel to memory
+CALL loadKernel
+
+MOV DI, kernelLoadedMessage
+CALL printStr
 
 ; Enable A20 line
-;MOV AL, 0xAD             ; Disable keyboard interrupts
-;OUT 0x64, AL             ; Send command to keyboard controller
-;MOV AL, 0xD0             ; Read status register
-;OUT 0x64, AL             ; Send command
-;IN AL, 0x60              ; Read status register
-;OR AL, 0x2               ; Set bit 1 (A20 line)
-;OUT 0x60, AL             ; Write to status register
+MOV AL, 0xAD             ; Disable keyboard interrupts
+OUT 0x64, AL             ; Send command to keyboard controller
+
+MOV AL, 0xD0             ; Read status register
+OUT 0x64, AL             ; Send command
+IN AL, 0x60              ; Read status register
+OR AL, 0x2               ; Set bit 1 (A20 line)
+OUT 0x60, AL             ; Write to status register
+
+; Check if A20 line is enabled
+MOV AL, 0xD0             ; Read status register
+OUT 0x64, AL             ; Send command
+IN AL, 0x60              ; Read status register
+AND AL, 0x2              ; Check if bit 1 is set
+JZ A20Fail               ; If not, jump to A20Fail
+MOV DI, A20Success
+CALL printStr
 
 ; Disable interrupts
-;CLI
+CLI
 
 ; Load Global Descriptor Table
-;LGDT [GDTDescriptor]
+LGDT [GDTDescriptor]
+MOV DI, GDTMessage
+CALL printStr
 
 ; Switch to protected mode
-;MOV EAX, CR0
-;OR EAX, 0x1
-;MOV CR0, EAX
+MOV EAX, CR0
+OR EAX, 0x1
+MOV CR0, EAX
 
-;JMP 0x08:protectedModeStart
+JMP 0x08:protectedModeStart
 
 
 ; messages
-message1 db 'Loaded stage 1', 0
-ERROR db 'Error in stage 1', 0
-
-
+message1 db 'Loaded stage 1', 0x0A, 0x0D, 0
+readError db 'Error reading kernel', 0x0A, 0x0D, 0
+A20Success db 'A20 line enabled', 0x0A, 0x0D, 0
+A20FailMessage db 'A20 line failed', 0x0A, 0x0D, 0
+GDTMessage db 'GDT loaded', 0x0A, 0x0D, 0
+ProtectedModeMessage db 'Switched to protected mode', 0x0A, 0x0D, 0
+kernelLoadedMessage db 'Kernel loaded', 0x0A, 0x0D, 0
 ; functions
-;protectedModeStart:
-;  MOV AX, 0x10
-;  MOV DS, AX
-;  MOV ES, AX
-;  MOV FS, AX
-;  MOV GS, AX
-;  MOV SS, AX
-;
-;  ; Jump to kernel
-;  MOV EAX, 0x00100000  ; Kernel is located at 1MB
-;  JMP EAX
-;
-;code_segment:
-;  dw 0xFFFF            ; Limit (0-15 bits)
-;  dw 0x0000            ; Base (0-15 bits)
-;  db 0x9A              ; Type and Attributes (Code Segment, Executable, Readable, Privilege Level 0)
-;  db 0xCF              ; Type and Attributes (Segment present, 32-bit segment, Granularity)
-;  dw 0x0000            ; Base (16-23 bits)
-;
-;data_segment:
-;  dw 0xFFFF            ; Limit (0-15 bits)
-;  dw 0x0000            ; Base (0-15 bits)
-;  db 0x92              ; Type and Attributes (Data Segment, Writable, Privilege Level 0)
-;  db 0xCF              ; Type and Attributes (Segment present, 32-bit segment, Granularity)
-;  dw 0x0000            ; Base (16-23 bits)
+
+protectedModeStart:
+  mov ax, 0x10
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
+  hlt
+
+loadKernel:
+  ; Load kernel to memory
+  MOV BX, 0x1000
+  MOV CX, 4
+  MOV AH, 0x02
+  MOV AL, 16
+  MOV CH, 0x00
+  MOV CL, 0x03
+  MOV DH, 0x00
+  INT 0x13
+  JC ReadError
+
+A20Fail:
+  MOV DI, A20FailMessage
+  CALL printStr
+  HLT
 
 printChar:
   MOV AH, 0x0E 
@@ -105,22 +118,47 @@ printStr:
   exitFn:
     RET
 
-;ReadError:
+ReadError:
   ; If something goes wrong, hang
-;  MOV DI, ERROR
-;  CALL printStr
-;  CLI
+  MOV DI, readError
+  CALL printStr
+  MOV AL, AH
+  CALL printHex
+  CLI
+  HLT
+
+printHex:
+  PUSH AX
+  MOV CL, 4
+
+  MOV BL, AL
+  SHR BL, CL
+  CALL printHexDigit
+  MOV BL, AL
+  AND BL, 0x0F
+  CALL printHexDigit
+  POP AX
+  RET
+printHexDigit:
+  ADD AL, '0'
+  CMP AL, '9'
+  JBE printHexDigitDone
+  ADD AL, 7
+printHexDigitDone:
+  MOV AL,BL
+  CALL printChar
+  RET
+
 
 ;section .data
-;GTDstart:
-;  dq 0x0000000000000000           ; Limit for GDT
-;  dq 0x00CF9A000000FFFF           ; Code segment descriptor
-;  dq 0x00CF92000000FFFF           ; Data segment descriptor
-;GTDend:
-;
-;GDTDescriptor:
-;  dw GTDend - GTDstart - 1      ; Size of the GDT:W
-;  dd GTDstart                   ; Pointer to the GDT
-;
+GTD:
+  dq 0x0000000000000000           ; Limit for GDT
+  dq 0x00CF9A000000FFFF           ; Code segment descriptor
+  dq 0x00CF92000000FFFF           ; Data segment descriptor
+
+GDTDescriptor:
+  dw GTD - GTD - 1      ; Size of the GDT
+  dd GTD                ; Pointer to the GDT
+
 TIMES 510 - ($ - $$) db 0 ; dont ask why 330 
 DW 0xAA55
